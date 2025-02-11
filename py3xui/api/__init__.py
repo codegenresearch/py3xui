c811351dbd92f4aeffb44011bff77d21a80c3948
venkatkrishna <venkatkrishna@moe>
2025-02-11 10:52:42 +0100

    Reset to 9f215316f9e1c1d16850d87d62e0a80123a2d1ae and made changes for LLM Round 1

--- py3xui/api/__init__.py
+++ py3xui/api/__init__.py
@@ -1,10 +1,4 @@
-from py3xui.api.api_client import ClientApi  # Provides methods to interact with the client-related functionalities of the XUI API.
-from py3xui.api.api_database import DatabaseApi  # Provides methods to interact with the database-related functionalities of the XUI API.
-from py3xui.api.api_inbound import InboundApi  # Provides methods to interact with the inbound-related functionalities of the XUI API.
-
-# Example usage:
-# api_client = ClientApi(host="https://xui.example.com", username="username", password="password")
-# api_database = DatabaseApi(host="https://xui.example.com", username="username", password="password")
-# api_inbound = InboundApi(host="https://xui.example.com", username="username", password="password")
-
-# These instances can be used to perform operations specific to clients, database, and inbounds respectively.
+# pylint: disable=missing-module-docstring
+from py3xui.api.api_client import ClientApi
+from py3xui.api.api_database import DatabaseApi
+from py3xui.api.api_inbound import InboundApi
